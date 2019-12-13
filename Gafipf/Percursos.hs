5b0ea9ceba428e2f0e09862381dd0da38c6de841
Mafalda Paço <mafalda.paco@gmail.com>
2019-12-13 17:09:18 +0000

    'Comentários'

--- Gafipf/Percursos.hs
+++ Gafipf/Percursos.hs
@@ -18,6 +18,7 @@ data Percurso = Percurso {
 
 type Ponto = (Double, Double)
 
+--Creates a template of json file
 toJson :: Percurso -> String
 toJson p = "{\n\"Categoria\": \"" ++ getCategoria p ++ "\",\n" ++ 
     "\"Tempo total (m)\":" ++ show (getTempo p) ++ ",\n\"Ganho acumulado\":" ++ 
@@ -25,10 +26,11 @@ toJson p = "{\n\"Categoria\": \"" ++ getCategoria p ++ "\",\n" ++
     show (getGanhoMetro p) ++ ",\n\"Pontos de Interesse\": [\"" ++ 
     intercalate "\",\n\t\"" (sitios p) ++ "\"]\n}"
 
-    
+ --Creates a route   
 criaPercurso :: [Coordenada] -> [Poi] -> Percurso
 criaPercurso coordenadas pois = Percurso coordenadas (getPoisInPercurso coordenadas pois)
 
+--Hiking difficulty
 getCategoria :: Percurso -> String
 getCategoria p
     | (dist < 10000) && (alt < 500) = "A"
@@ -39,44 +41,56 @@ getCategoria p
       dist = getDistancia p
       alt = getGanho p
 
+--The amount of time it takes to hike the route
 getTempo :: Percurso -> Int
 getTempo p = fromIntegral (getTime (last (coordenadas p)) - fromIntegral (getTime (head (coordenadas p)))) `div` 60
 
+--How much you'll climb in the hike
 getGanho :: Percurso -> Int
 getGanho p = round (getGanhoAltitude (map getAltitude (coordenadas p)))
 
+--Returns the gain in height between multiple heights
 getGanhoAltitude :: [Double] -> Double
 getGanhoAltitude [] = 0
 getGanhoAltitude [x] = 0
 getGanhoAltitude [x,s] = if x < s then s-x else 0
 getGanhoAltitude (x:s:xs) = (if x < s then s-x else 0) + getGanhoAltitude (s:xs)
 
+--How much you'll climb for each metre you walk horizontally
 getGanhoMetro :: Percurso -> Double
 getGanhoMetro p = round3dp (getGanhoAltitude (map getAltitude (coordenadas p)) / getDistancia p)
 
+--Rounds numbers to their 3 decimal place
 round3dp :: Double -> Double
 round3dp x = fromIntegral (round $ x * 1e3) / 1e3
 
+--Returns the names of the places visited
 getSitios :: Percurso -> [String]
 getSitios p = map read (sitios p)
 
+--Returns the names of the places visited
 getDistancia :: Percurso -> Double
 getDistancia p = 85000 * distancia (map getPoint (coordenadas p))
 
+--Points of interest from the route
 getPoisInPercurso :: [Coordenada] -> [Poi] -> [String]
 getPoisInPercurso cs ps = [getLocal x | x <- ps, isPoiInPercurso cs x]
 
+--Checks if a certain point of interest is visited in the route
 isPoiInPercurso :: [Coordenada] -> Poi -> Bool
 isPoiInPercurso c p = 
     let ponto = getPonto p
     in foldl (\acc x -> inNeighbourhood ponto x || acc) False (getPoints c)
 
+--Checks if a certain place is visited or if the hike just goes through it's neighbourhood
 inNeighbourhood :: Ponto -> Ponto -> Bool
 inNeighbourhood p1 p2 = getNumResolution (fst p1) > distancia2Pontos p1 p2
 
+--Returns the distance between 2 points
 distancia2Pontos :: Ponto -> Ponto -> Double
 distancia2Pontos (x1, y1) (x2, y2) = sqrt ((x2 - x1) ^ 2 + (y2 - y1) ^ 2)
 
+--Returns the distance between multiple points
 distancia :: [(Double, Double)] -> Double
 distancia [] = 0
 distancia [x] = 0
@@ -86,5 +100,6 @@ distancia (x:s:xs) = distancia2Pontos x s + distancia (s:xs)
 isInt :: (RealFrac a) => a -> Bool
 isInt x = x == fromInteger (round x)
 
+--Returns the number resolution
 getNumResolution :: (RealFrac a) => a -> a
 getNumResolution num = last [10^^x | x <- [(-10)..0], isInt (num / 10^^x)]
--- Main.hs
+++ Main.hs
@@ -25,20 +25,26 @@ handler e
                                  Nothing -> putStrLn "Whoops! File does not exist at unknown location!"  
     | otherwise = ioError e   
 
+--Builds a json file
 buildJson :: String -> String -> String -> IO ()
 buildJson percurso pontos jsonPath = writeFile jsonPath (toJson (buildPercurso percurso pontos))
 
+--Builds the route from all the processed information, ready to be submitted to the GAFIPF website
 buildPercurso :: String -> String -> Percurso
 buildPercurso c p = criaPercurso (buildCoordenadas c) (buildPois p)
 
+--Separates strings by line (\n), then separates them by comma (,) and proceeds to create coordinates with the latitude, longitude, altitude and time
 buildCoordenadas :: String -> [Coordenada]
 buildCoordenadas = map (buildCoordenadaFromStrings . splitCsvValues) . lines
 
+--Returns the coordinates from a string
 buildCoordenadaFromStrings :: [String] -> Coordenada
 buildCoordenadaFromStrings (x:s:a:t) = criaCoordenada (read x :: Double, read s :: Double) (read a :: Double) (readTime (head t))
 
+--Separates strings by line (\n), then separates them by comma (,) and proceeds to create points of interest from their latitude, longitude and name
 buildPois :: String -> [Poi]
 buildPois = map (buildPoiFromStrings . splitCsvValues) . lines
 
+--Returns the point of interest from a string
 buildPoiFromStrings :: [String] -> Poi
 buildPoiFromStrings (x:s:xs) = criaPoi (read x, read s) (head xs)
